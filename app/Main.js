191b33d288fe9816e162c668b68eb988e353898c
sobaboy <1214hso@naver.com>
2022-10-26 12:08:57 +0900

    initial commit

---
+++ app/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// My Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+function Main() {
+  return (
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
+
+if (module.hot) {
+  module.hot.accept();
+}
